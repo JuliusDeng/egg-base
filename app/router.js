cbae918eba435cd1914c380c8517808db4eef7f9
JuliusDeng <1576554007@qq.com>
2020-11-23 17:15:24 +0800

    资源路由和路由分组

--- app/router.js
+++ app/router.js
@@ -11,5 +11,13 @@ module.exports = app => {
   router.get("/user/list", controller.user.index)
   router.get("/user/read/:id", controller.user.read)
   router.post("/user/create", controller.user.create)
+
+  // 资源路由
+  // router.resources('post', '/api/post', controller.post);
+  
+  // 路由分组--从外部引入
+  require("./router/post")(app)
+  require("./router/user")(app)
+
 };
 
