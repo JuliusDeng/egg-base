7a0e262cce5823c8d0632b3a1523cdeb5e2e29dd
JuliusDeng <1576554007@qq.com>
2020-11-24 17:40:10 +0800

    sequelize模型--单个查询

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,27 +1,7 @@
 'use strict';
 
 const Controller = require('egg').Controller;
-let demo = [{
-    id: 1,
-    username: "用户名1",
-    nickname: "昵称",
-    sex: "男"
-}, {
-    id: 2,
-    username: "用户名2",
-    nickname: "昵称",
-    sex: "男"
-}, {
-    id: 3,
-    username: "用户名3",
-    nickname: "昵称",
-    sex: "男"
-}, {
-    id: 4,
-    username: "用户名4",
-    nickname: "昵称",
-    sex: "男"
-}];
+
 class UserController extends Controller {
     // 用户列表
     async index() {
@@ -40,8 +20,24 @@ class UserController extends Controller {
 
     // 读取某个用户数据
     async read() {
-        let id = this.ctx.params.id;
-        let detail = demo.find(item => item.id == id);
+        let id = parseInt(this.ctx.params.id);
+        // 通过主键查询单个数据
+        // let detail = await this.app.model.User.findByPk(id);
+        // if (!detail) {
+        //     return this.ctx.body = {
+        //         msg: "fail",
+        //         data: "用户不存在啊"
+        //     }
+        // }
+
+
+        let detail = await this.app.model.User.findOne({
+            where: {
+                id,
+                sex: "女"
+            }
+        })
+
         this.ctx.body = {
             msg: 'ok',
             data: detail
@@ -53,11 +49,46 @@ class UserController extends Controller {
         // 参数验证
         // 写入数据库
 
-        let res = await this.app.model.User.create({
-            username: "我小黎啊",
-            password: "123456",
-            sex: "女"
-        });
+        // 单个新增
+        // let res = await this.app.model.User.create({
+        //     username: "我小黎啊",
+        //     password: "123456",
+        //     sex: "女"
+        // });
+
+        // 批量新增
+        let res = await this.app.model.User.bulkCreate([
+            {
+                username: "黎小左",
+                password: "密码1",
+                sex: "女"
+            },
+            {
+                username: "邓小右",
+                password: "密码2",
+                sex: "男"
+            },
+            {
+                username: "黎小左2",
+                password: "密码3",
+                sex: "女"
+            },
+            {
+                username: "邓小右2",
+                password: "密码4",
+                sex: "男"
+            },
+            {
+                username: "黎小左3",
+                password: "密码5",
+                sex: "女"
+            },
+            {
+                username: "邓小右3",
+                password: "密码6",
+                sex: "男"
+            },
+        ])
 
         // 成功
         this.ctx.body = {
--- egg-words.md
+++ egg-words.md
@@ -1,2 +1,3 @@
 # egg-words
 ## param [ˌpærəˈm]  abbr. 参数 ##  Migrations [maɪˈɡreɪʃnz]  数据库迁移
+## bulk [bʌlk] 批量;大批 unique [juˈniːk] adj. 独特的，稀罕的
